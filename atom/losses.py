e9573b34995826ed2292e3ef1bcc4933295612cd
FelipeOG1 <feliog1234@gmail.com>
2026-02-09 19:07:57 -0600

    New const and new loss function

---
+++ atom/losses.py
@@ -0,0 +1,24 @@
+import numpy as np
+from dataclasses import dataclass,field
+from atom.consts import EPS
+
+@dataclass
+class BinaryCrossentropy:
+    y_hat:np.ndarray
+    y:np.ndarray
+
+    def compute_loss(self):return -self.y*np.log(self.y_hat) - (1-self.y) * np.log(1-self.y_hat)
+    
+    def compute_cost(self):return np.mean(self.compute_loss())
+
+@dataclass
+class SparseCategoricalCrossentropy:
+    y_hat:np.ndarray
+    y:np.ndarray
+
+    def compute_loss(self):
+        eps = 1e-9
+        probs = self.y_hat[np.arange(len(self.y)),self.y]
+        return -np.log(probs + eps)
+
+

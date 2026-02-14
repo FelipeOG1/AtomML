d3e11a49179ebdd141568685b40093b700e94215
FelipeOG1 <feliog1234@gmail.com>
2026-02-14 17:28:33 -0600

    test __mul__

--- test/test_engine.py
+++ test/test_engine.py
@@ -14,3 +14,16 @@ class TestScalar:
         assert a in d._prev
         assert b in d._prev
         
+    def test_mul(self):
+        a = Scalar(3.0)
+        b = Scalar(2.0)
+        
+        d = a * b
+
+        
+        assert d.data == 6.0
+        assert a in d._prev
+        assert b in d._prev
+    
+        
+        

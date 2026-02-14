e6e0c64de93ced63453612fe37c36b75ca81590a
FelipeOG1 <feliog1234@gmail.com>
2026-02-14 17:13:14 -0600

    Spacing

--- atom/losses.py
+++ atom/losses.py
@@ -4,17 +4,16 @@ from atom.constants import EPS
 
 @dataclass
 class BinaryCrossentropy:
-    y_hat:np.ndarray
-    y:np.ndarray
-
+    y_hat: np.ndarray
+    y: np.ndarray
     def compute_loss(self):return -self.y*np.log(self.y_hat) - (1-self.y) * np.log(1-self.y_hat)
     
     def compute_cost(self):return np.mean(self.compute_loss())
 
 @dataclass
 class SparseCategoricalCrossentropy:
-    y_hat:np.ndarray
-    y:np.ndarray
+    y_hat: np.ndarray
+    y: np.ndarray
 
     def compute_loss(self):
         probs = self.y_hat[np.arange(len(self.y)),self.y]
